17273b0682357eb1d063036a08560771161a45b7
Peverous <xristos4870@gmail.com>
2017-04-19 13:12:30 +0300

    Final Edition

---
+++ EmbeddedLab_SW/Debug/src/crc.d
@@ -0,0 +1,63 @@
+src/crc.o src/crc.d: ../src/crc.c ../src/crc.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xparameters.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xtmrctr.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_types.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_assert.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xstatus.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xtmrctr_l.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_io.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/mb_interface.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_exception.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_printf.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_exception.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_printf.h ../src/Defs.h \
+ ../src/Timer.h ../src/Interrupts.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xintc.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xparameters.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xintc_l.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xuartlite.h ../src/UART.h \
+ ../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_io.h
+
+../src/crc.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xparameters.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xtmrctr.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_types.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_assert.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xstatus.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xtmrctr_l.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_io.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/mb_interface.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_exception.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_printf.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_exception.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_printf.h:
+
+../src/Defs.h:
+
+../src/Timer.h:
+
+../src/Interrupts.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xintc.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xparameters.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xintc_l.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xuartlite.h:
+
+../src/UART.h:
+
+../../EmbeddedLab_SW_bsp/microblaze_0/include/xil_io.h:
---
+++ README.md
@@ -0,0 +1,1 @@
+this is a project for the Embedded Communication Systems @ Electrical Enginner of University of Patra 
